4bc64090e2e737b3f39e8bd1ee7a22b0acb00a40
dabin1203 <__@example.com>
2026-03-25 01:37:28 +0900

    test: add cart functionality test using InventoryPage

---
+++ tests/test_inventory.py
@@ -0,0 +1,14 @@
+from playwright.sync_api import expect
+from pages.login_page import LoginPage       
+from pages.inventory_page import InventoryPage
+
+def test_add_backpack_to_cart(page):
+    login_page = LoginPage(page)
+    inventory_page = InventoryPage(page)
+
+    login_page.navigate()
+    login_page.login("standard_user", "secret_sauce")
+
+    inventory_page.add_backpack_to_cart()
+
+    expect(inventory_page.cart_badge).to_have_text("1")
