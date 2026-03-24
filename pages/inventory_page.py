883a2dd56cac9c9fba6188d5234b7e9e8d8c8424
dabin1203 <__@example.com>
2026-03-25 01:37:14 +0900

    feat: add InventoryPage object and locators

---
+++ pages/inventory_page.py
@@ -0,0 +1,8 @@
+class InventoryPage:
+    def __init__(self, page):
+        self.page = page
+        self.add_to_cart_btn = page.locator("[data-test='add-to-cart-sauce-labs-backpack']")
+        self.cart_badge = page.locator(".shopping_cart_badge")
+
+    def add_backpack_to_cart(self):
+        self.add_to_cart_btn.click()
